bec40bb084a057ce0403201598285ad315341a98
Hristo Lakov <107894257+Miserry@users.noreply.github.com>
2022-06-28 15:27:22 +0300

    Add files via upload

---
+++ sequence_2k.py
@@ -0,0 +1,21 @@
+
+# number = int(input())
+# total = 0
+#
+# while total < number:
+#     total = total * 2 + 1
+#     print(total)
+#
+#====================================================
+
+
+num = int(input())
+total = 1
+
+while total <= num:
+    print(total)
+    total = 2 * total + 1
+
+
+
+
